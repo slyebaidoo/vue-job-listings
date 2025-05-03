4c3bc569079b2eea418d82a79954041bcbf6b583
slye <slye.dev@gmail.com>
2025-05-03 14:49:51 +0200

    Intial setup add: app.vue - test file add: theme_files feat: navbar

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/*.{vue,js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

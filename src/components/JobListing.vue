46ceb1e1bf4cb0b5f568c2fcab72d818db32f20c
slye <slye.dev@gmail.com>
2025-05-09 14:20:05 +0200

    feat: Add:
    add computed and truncated

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,39 +1,63 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, computed } from "vue";
 
-defineProps({
-	job: Object,
+const props = defineProps({
+  job: Object,
+});
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + "...";
+  }
+  return description;
 });
 </script>
 
 <template>
-	<!-- Job Listing 1 -->
-	<div class="bg-white rounded-xl shadow-md relative">
-		<div class="p-4">
-			<div class="mb-6">
-				<div class="text-gray-600 my-2">{{ job.type }}</div>
-				<h3 class="text-xl font-bold">{{ job.title }}</h3>
-			</div>
+  <!-- Job Listing 1 -->
+  <div class="relative rounded-xl bg-white shadow-md">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="my-2 text-gray-600">{{ job.type }}</div>
+        <h3 class="text-xl font-bold">{{ job.title }}</h3>
+      </div>
 
-			<div class="mb-5">
-				{{ job.description }}
-			</div>
+      <div class="mb-5">
+        <div>
+          {{ truncatedDescription }}
 
-			<h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
+          <button
+            @click="toggleFullDescription"
+            class="hover: mb-5 text-green-500 hover:text-green-600"
+          >
+            {{ showFullDescription ? "Less" : "More" }}
+          </button>
+        </div>
+      </div>
 
-			<div class="border border-gray-100 mb-5"></div>
+      <h3 class="mb-2 text-green-500">{{ job.salary }} / Year</h3>
 
-			<div class="flex flex-col lg:flex-row justify-between mb-4">
-				<div class="text-orange-700 mb-3">
-					<i class="fa-solid fa-location-dot text-lg"></i>
-					{{ job.location }}
-				</div>
-				<a
-					:href="'/job/' + job.id"
-					class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
-					Read More
-				</a>
-			</div>
-		</div>
-	</div>
+      <div class="mb-5 border border-gray-100"></div>
+
+      <div class="mb-4 flex flex-col justify-between lg:flex-row">
+        <div class="mb-3 text-orange-700">
+          <i class="fa-solid fa-location-dot text-lg"></i>
+          {{ job.location }}
+        </div>
+        <a
+          :href="'/job/' + job.id"
+          class="h-[36px] rounded-lg bg-green-500 px-4 py-2 text-center text-sm text-white hover:bg-green-600"
+        >
+          Read More
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
